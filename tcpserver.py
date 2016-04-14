629abebde98cd3955857d077ba17962ec52b1869
Stephen Krings <kringz@gmail.com>
2016-04-13 22:21:36 -0400

    Simple TCP server

---
+++ tcpserver.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import socket
+import threading
+
+bind_ip = "0.0.0.0"
+bing_port = 9999
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind((bind_ip,bing_port))
+
+server.listen(5)
+
+print "[*] Listening on %s:%d" % (bind_ip,bing_port)
+
+# client handling thread
+
+def handle_client(client_socket):
+
+	# print out what the client send
+	requests = clien_socket.recv(1024)
+
+	print "[*] Received: %s" % request
+	
+
+	# send a packet back
+
+	client_socket.send("ACK!")
+
+	client_socket.close()
+
+while True:
+
+	client,addr = server.accept()
+
+	print "[*] Accepted Connection from %s:%d" % (addr[0],addr[1])
+
+	# client thread to handle incoming data
+	
+	client_handler = threading.Thread(target=handle_client,args=(client,)) 
+	client_handler.start()
