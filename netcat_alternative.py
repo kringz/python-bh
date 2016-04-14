52b092daa2578377b3e5c9cc8a360b48894ac073
Stephen Krings <kringz@gmail.com>
2016-04-14 16:56:05 -0400

    .

--- netcat_alternative.py
+++ netcat_alternative.py
@@ -88,3 +88,112 @@ def main():
 		server_loop)_
 
 main()
+
+def client_sender(buffer):
+	
+	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		
+	try:
+
+		# connect to our target host
+		client.connect((target,port))
+	
+		if len(buffer):
+			client.send(buffer)
+		while True:
+		
+			# now wait for data back
+			recv_len = 1
+			response = ""
+		
+			while recv_len:
+		
+				data		= client.recv(4096)
+				recv_len	= len(data)
+				response	+= data
+				
+				if recv_len < 4096
+					break
+				
+				print response,
+			
+				# wait for more input
+				buffer = raw_input("")
+				buffer += "\n"
+	
+				# send it off
+			
+				client.send(buffer)
+
+		except:
+		
+			print "[*] Exception! Exiting."
+		
+			# tear down the connection
+			client.close()
+
+def server_loop():	
+	global target
+
+	# if no target is defined, we listed on all interfaces
+	if not len(target):
+		target = "0.0.0.0"
+	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server.bind((target,port))
+	server.listen(5)
+
+	whilte True:
+		client_socket, addr = server.accept()
+			
+		# spin off a thread to handle our new client
+		
+		client_thread = threading.Tread(target=client_handler, args=client_socket,))
+		client_threat.start()
+
+def run_command(command):
+	
+	# trim the newline
+		
+	command.command.rstrip()
+	
+	# run the command and get the output back
+	
+	try:
+		output = subprocess.check_output(command,stderr=subprocess.STOUT, shell=True)
+	except:
+		output = "Failed to execute command. \r\n"
+
+	# send the output back to the client
+	return output
+
+def client_handler(client_socket):
+	
+	global upload
+	global execute
+	global command
+
+	# check for upload
+	if len(upload_destination):
+	
+	# read in all of the bytes and write to our destination
+	
+		file_buffer = ""
+		
+		# keep reading data until none is available
+
+		while True:
+			data = client_socket.recv(1024)
+		
+			if not data:
+				break
+			else:
+				file_buffer += data
+		# now we take these bytes and try to write them out
+			
+		try:
+			file_descriptor = open(upload_destination, "wb")
+			file_descriptor = write(file_buffer)
+			file_descriptor.close()
+
+			# acknowledge that we wrote the file out
+			client_socket.send("Successfully saved file to %s\r\n" % upload_destination)
